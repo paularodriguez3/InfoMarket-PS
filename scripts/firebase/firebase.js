36886e7afce77d5c8bfe6d9402abc1344d6f2b63
kevinjfa <kevin.fa7@outlook.com>
2025-03-16 20:17:10 +0000

    Changed firebase script with new functions to use Firestore Database, not Realtime.

--- scripts/firebase/firebase.js
+++ scripts/firebase/firebase.js
@@ -1,56 +1,81 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getDatabase, ref, onValue, set, update, remove } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js"
-import { firebaseConfig } from "/InfoMarket-PS/config.js";
+import { getFirestore, doc, getDoc, collection, query, getDocs, addDoc, deleteDoc, updateDoc, where } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { firebaseConfig, inicioSesion, inicioDeSesion } from "../../config.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase();
+const database = getFirestore(app);
+const auth = getAuth(app);
 
+await inicioDeSesion(auth);
 
-// Esta función lee los datos de la ruta especificada de la realtime database
-export const leerDatos = (ruta) => {
-    return new Promise((resolve, reject) => {
-        const dbRef = ref(database, ruta);
-        onValue(dbRef, (snapshot) => {
-            const data = snapshot.val();
-            resolve(data);
-        }, (error) => {
-            reject(error);
-        });
-    });
+// Esta función lee los datos de la colección especificada y los devuelve en un objeto id : datos
+export const readCollection = async (col) => {
+    const colRef = query(collection(database, col));
+    const colSnap = await getDocs(colRef);
+    if (!colSnap.empty) {
+        let data = {};
+        colSnap.forEach((item) => {data[item.id] = item.data()});
+        return data;
+    } else {
+        console.log("No existe esa colección.");
+    }
 };
 
-// Esta función escribe y/o modifica el datoAModificar en la realtime database para que tenga el valor pasado por parámetro
-export const escribirDatos = (ruta, datoAModificar, valor) => {
-    return new Promise((resolve, reject) => {
-        try {
-            const dbRef = ref(database, ruta);
-            const dataToUpdate = { [datoAModificar]: valor };
+// Esta función lee los datos del documento solicitado de la colección especificada y lo devuelve.
+export const readDoc = async (col, document) => {
+    const docRef = doc(database, col, document);
+    const docSnap = await getDoc(docRef);
 
-            console.log('Data to be updated:', dataToUpdate);
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else {
+        console.log("No existe el documento");
+    }
+}
 
-            update(dbRef, dataToUpdate)
-                .then(() => {
-                    console.log('Dato actualizado correctamente');
-                    resolve();
-                })
-                .catch((error) => {
-                    console.error('Error al actualizar el dato:', error);
-                    reject(error);
-                });
-        } catch (error) {
-            console.error('Error en la función escribirDatos:', error);
-            reject(error);
-        }
+// Esta función recibe datos en formato object y crea un documento en la colección especificada,
+// con los datos especificados. Además, devuelve la referencia a ese documento.
+export const createDocOnCollection = async (col, data) => {
+    const docRef = await addDoc(collection(database, col), data);
+    return docRef.id;
+}
+
+// Esta función actualiza un documento, añadiendo campos o modificando aquellos ya existentes
+export const updateDocOnCollection = async (col, id, data) => {
+    const docRef = doc(database, col, id);
+    await updateDoc(docRef, data);
+}
+
+// Esta función elimina un documento de la colección especificada.
+export const deleteDocOnCollection = async (col, document) => {
+    await deleteDoc(doc(database, col, document));
+}
+
+// Esta función filtra por el campo solicitado y devuelve un objeto ID : datos con los objetos cuyo campo sea igual al
+// tercer parámetro.
+export const filterEqualsByFieldOnCollection = async (col, field, equals) => {
+    const q = query(collection(database, col), where(field, "==", equals));
+    const querySnapshot = await getDocs(q);
+    const data = {}
+    querySnapshot.forEach((doc) => {
+        data[doc.id] = doc.data();
     });
+    return data;
 }
 
-// Esta función sirve para eliminar datos de la realtime database
-export const eliminarDatos = (ruta, datoAEliminar) => {
-    return escribirDatos(ruta, datoAEliminar, null);
+// Esta función filtra la colección pasada por parámetro según si el field cumple el filtro pasado por parámetro,
+// junto con el value a comprobar.
+export const filterByFieldOnCollection = async (col, field, filter, value) => {
+    const q = query(collection(database, col), where(field, filter, value));
+    const querySnapshot = await getDocs(q);
+    const data = {}
+    querySnapshot.forEach((doc) => {
+        data[doc.id] = doc.data();
+    });
+    return data;
 }
-
-
