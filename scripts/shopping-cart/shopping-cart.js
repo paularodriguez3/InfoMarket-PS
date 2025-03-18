e7cfe910606784cf25de95771fff226b62874737
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-18 15:43:42 +0000

    Added function addToCart().

--- scripts/shopping-cart/shopping-cart.js
+++ scripts/shopping-cart/shopping-cart.js
@@ -3,5 +3,9 @@ const shoppingCart = [];
 function showShoppingCart() {
     shoppingCart.forEach(item => {
         console.log(item);
-    })
+    });
+}
+
+function addToCart(item) {
+    shoppingCart.push(item);
 }
