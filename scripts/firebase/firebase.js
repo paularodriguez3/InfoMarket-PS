3995ff5f80dbc404778c27e979e535654e237b26
kevinjfa <kevin.fa7@outlook.com>
2025-03-18 15:12:24 +0000

    Fixed auth error on firebase script.

--- scripts/firebase/firebase.js
+++ scripts/firebase/firebase.js
@@ -11,10 +11,9 @@ const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
 const auth = getAuth(app);
 
-await inicioDeSesion(auth);
-
 // Esta función lee los datos de la colección especificada y los devuelve en un objeto id : datos
 export const readCollection = async (col) => {
+    await inicioDeSesion(auth);
     const colRef = query(collection(database, col));
     const colSnap = await getDocs(colRef);
     if (!colSnap.empty) {
@@ -28,6 +27,7 @@ export const readCollection = async (col) => {
 
 // Esta función lee los datos del documento solicitado de la colección especificada y lo devuelve.
 export const readDoc = async (col, document) => {
+    await inicioDeSesion(auth);
     const docRef = doc(database, col, document);
     const docSnap = await getDoc(docRef);
 
@@ -41,24 +41,28 @@ export const readDoc = async (col, document) => {
 // Esta función recibe datos en formato object y crea un documento en la colección especificada,
 // con los datos especificados. Además, devuelve la referencia a ese documento.
 export const createDocOnCollection = async (col, data) => {
+    await inicioDeSesion(auth);
     const docRef = await addDoc(collection(database, col), data);
     return docRef.id;
 }
 
 // Esta función actualiza un documento, añadiendo campos o modificando aquellos ya existentes
 export const updateDocOnCollection = async (col, id, data) => {
+    await inicioDeSesion(auth);
     const docRef = doc(database, col, id);
     await updateDoc(docRef, data);
 }
 
 // Esta función elimina un documento de la colección especificada.
 export const deleteDocOnCollection = async (col, document) => {
+    await inicioDeSesion(auth);
     await deleteDoc(doc(database, col, document));
 }
 
 // Esta función filtra por el campo solicitado y devuelve un objeto ID : datos con los objetos cuyo campo sea igual al
 // tercer parámetro.
 export const filterEqualsByFieldOnCollection = async (col, field, equals) => {
+    await inicioDeSesion(auth);
     const q = query(collection(database, col), where(field, "==", equals));
     const querySnapshot = await getDocs(q);
     const data = {}
@@ -71,6 +75,7 @@ export const filterEqualsByFieldOnCollection = async (col, field, equals) => {
 // Esta función filtra la colección pasada por parámetro según si el field cumple el filtro pasado por parámetro,
 // junto con el value a comprobar.
 export const filterByFieldOnCollection = async (col, field, filter, value) => {
+    await inicioDeSesion(auth);
     const q = query(collection(database, col), where(field, filter, value));
     const querySnapshot = await getDocs(q);
     const data = {}
