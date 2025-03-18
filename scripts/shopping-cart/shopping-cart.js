e3749e0d068b1f7c58bf30a90db6aa6a414f89c4
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-18 16:36:54 +0000

    Implemetned function addToCart.

--- scripts/shopping-cart/shopping-cart.js
+++ scripts/shopping-cart/shopping-cart.js
@@ -1,11 +1,36 @@
+import { readCollection } from "../firebase/firebase.js";
+
 const shoppingCart = [];
 
-function showShoppingCart() {
+// PRUEBA CON LA BASE DE DATOS
+document.addEventListener("DOMContentLoaded", () => {
+    readCollection("productos").then(products =>  {
+        for (const product of Object.keys(products)) {
+            addToCart(products[product]);
+        }
+    showShoppingCart();
+    });
+});
+// =============================
+
+function showShoppingCart() { // TODO: Finish showShoppingCart()
+    // Obtener shopping-cart-component
+    let shoppingCartComponent;
+
+    // Vaciar la lista de shoppingCartComponent
+
     shoppingCart.forEach(item => {
         console.log(item);
+
+        // Obtener product-info-component
+         let productInfoComponent;
+
+        // Modificar productInfoComponent para que sus datos coincidan con item
+
+        // Añadir productInfoComponent a shoppingCartComponent
+
     });
 }
-
 function addToCart(item) {
     shoppingCart.push(item);
 }
