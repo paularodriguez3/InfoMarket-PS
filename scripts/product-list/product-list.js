556ba02e7aad33bbec623e254a3190542c5dc5b4
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2025-03-23 13:55:02 +0000

    Product list shows products from database

--- scripts/product-list/product-list.js
+++ scripts/product-list/product-list.js
@@ -1,42 +1,39 @@
-import {createDocOnCollection, readCollection, readDoc, filterEqualsByFieldOnCollection, deleteDocOnCollection, updateDocOnCollection } from "/scripts/firebase/firebase.js";
+import {createDocOnCollection, readCollection, readDoc, filterEqualsByFieldOnCollection, deleteDocOnCollection, updateDocOnCollection, getImageUrl } from "../../scripts/firebase/firebase.js";
 
 async function cargarComponenteProducto() {
     const response = await fetch("../templates/product-component/product-component.html");
     const text = await response.text();
 
-    // Crear un contenedor temporal para insertar el template
     const contenedor = document.createElement("div");
     contenedor.innerHTML = text;
 
-    // Insertar el template en el `body` del documento
     document.body.appendChild(contenedor);
 }
 
-export async function obtenerProductos(categoria, subcategoria, subcategoria2) {
+export async function obtenerProductos(categoria) {
     await cargarComponenteProducto();
     const productosGrid = document.getElementById("product-grid");
     const template = document.getElementById("product-template").content;
 
-    const productos = await readCollection(categoria, subcategoria, subcategoria2);
+    const productos = await readCollection(categoria);
 
     console.log("productos");
 
-    Object.entries(productos).forEach(([id, productoData]) => {
+    for (const [id, productoData] of Object.entries(productos)) {
 
-        // Clonar la plantilla del producto
         const productoElemento = document.importNode(template, true);
 
-        // Rellenar la información del producto
-        productoElemento.querySelector("#image").src = productoData.Imagen;
+        const imagen = await getImageUrl(productoData.Imagen);
+
+        productoElemento.querySelector("#image").src = imagen;
         productoElemento.querySelector("#product-name").textContent = productoData.Nombre;
-        productoElemento.querySelector("#product-desc").textContent = productoData.Desc;
+        //productoElemento.querySelector("#product-desc").textContent = productoData.Desc;
         productoElemento.querySelector("#price").textContent = productoData.Precio;
-        // Añadir al grid de productos
+
         productosGrid.appendChild(productoElemento);
-    });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // Llamar la función con los valores deseados de categoría y subcategorías
-    await obtenerProductos("productos", "Informática", "Ordenadores");
+    await obtenerProductos("productos/Informática/Ordenadores");
 });
