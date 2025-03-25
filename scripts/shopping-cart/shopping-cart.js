20faefe7975f920c7b0e4538a6668b4f0042bd78
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-25 16:49:07 +0000

    Added quantity buttons to shopping cart.

--- scripts/shopping-cart/shopping-cart.js
+++ scripts/shopping-cart/shopping-cart.js
@@ -1,5 +1,4 @@
-
-import { readCollection } from "../firebase/firebase.js";
+import {getImageUrl, readCollection} from "../firebase/firebase.js";
 
 
 const shoppingCart = [];
@@ -22,26 +21,31 @@ async function showShoppingCart() { // TODO: Finish showShoppingCart()
     const template = document.getElementById("product-info-template").content;
 
     // Obtener el elemento shopping-cart
-    const shoppingCartList = document.getElementById("shopping-cart");
-    shoppingCart.forEach(databaseElement => {
+    const shoppingCartList = document.getElementById("shopping-cart-list");
+    for (const databaseElement of shoppingCart) {
         for (let id in databaseElement) {
             let element = databaseElement[id];
             // Cargar el template
             const item = document.importNode(template, true);
-            console.log(item);
 
             // Modificar valores del template
-            console.log(element.Imagen);
-            item.querySelector("#image").src = element.Imagen;
+            item.querySelector("#image").src = await getImageUrl(element.Imagen);
             item.querySelector("#product-name").textContent = element.Nombre;
             item.querySelector("#product-desc").textContent = element.Desc;
             item.querySelector("#product-quantity").textContent = element.Quantity;
             item.querySelector("#product-price").textContent = element.Price;
 
+            // Añadir botones
+            const plus = item.getElementById("button-plus");
+            const minus = item.getElementById("button-minus");
+            console.log(plus.classList);
+            plus.classList.remove("hidden-button");
+            minus.classList.remove("hidden-button");
+            console.log(plus.classList);
             // Añadir componente
             shoppingCartList.appendChild(item);
         }
-    });
+    }
 }
 
 function addToCart(item) {
