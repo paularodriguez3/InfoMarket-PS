0d54c0397c2901a36199f008434f265e0d694584
Jesus <jesus.tdd03@gmail.com>
2025-03-29 13:12:34 +0000

    Added + and - button functionality.

--- scripts/product-details/product-details.js
+++ scripts/product-details/product-details.js
@@ -1,6 +1,8 @@
 import { getImageUrl } from "../firebase/firebase.js";
 import { addToCart } from "../shopping-cart/shopping-cart.js";
 
+let quantity= 1;
+
 document.addEventListener("DOMContentLoaded", async () => {
     const producto = JSON.parse(localStorage.getItem("productoSeleccionado"));
     //console.log(producto.id);
@@ -11,6 +13,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("#product-name").textContent = producto.data.Nombre;
     document.querySelector("#product-desc").textContent = producto.data.Descripcion;
     document.querySelector("#price").textContent = producto.data.Precio;
+    document.querySelector("#qty-value").textContent = quantity;
 
     const caracteristicas = document.getElementById("product-features");
 
@@ -22,9 +25,22 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const addToCartButton = document.getElementById("add-to-cart");
     addToCartButton.addEventListener("click", () => {
-        // console.log(producto);
-        addToCart(producto, 1);
+        addToCart(producto, quantity);
+    });
 
+    const plus = document.getElementById("plus");
+    plus.addEventListener("click", () => {
+        quantity++;
+        document.querySelector("#qty-value").textContent = quantity;
+        document.querySelector("#price").textContent = Math.round(parseFloat(producto.data.Precio)*quantity*100)/100;
+    });
 
+    const minus = document.getElementById("minus");
+    minus.addEventListener("click", () => {
+        if (quantity !== 1) {
+            quantity--;
+            document.querySelector("#qty-value").textContent = quantity;
+            document.querySelector("#price").textContent = Math.round(parseFloat(producto.data.Precio)*quantity*100)/100
+        }
     });
 });
