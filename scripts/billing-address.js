7f41943a3e65b1081cc86bf09689c987a8c5115a
Jesus <jesus.tdd03@gmail.com>
2025-03-30 19:12:24 +0100

    Fixed billing address.

--- scripts/billing-address.js
+++ scripts/billing-address.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const continueButton = document.getElementById("continue-button");
-    continueButton.addEventListener("click", () => {
-        window.location.href = "../screens/shipping-method.html";
+    continueButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        let canContinue = true;
+        document.querySelectorAll("input").forEach(input => {
+            if (input.placeholder === "¿Qué estás buscando?...") return;
+            if (input.value === "") {canContinue = false;}
+        });
+        if (canContinue) {
+            window.location.href = "../screens/shipping-method.html";
+        } else {
+            alert("Rellena todos los campos, por favor.")
+        }
     });
 });
