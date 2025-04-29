c55ed0928290530b85d55cb9e5bb3ac20c7a51d7
Jesus Toro <jesus.tdd03@gmail.com>
2025-04-29 16:12:30 +0100

    Fixed removeFromCart.

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -25,9 +25,9 @@ export class ShoppingCartService {
     this.saveCart(shoppingCart);
   }
 
-  removeFromCart(item: Product): void {
+  removeFromCart(item: ShoppingCartItem): void {
     const shoppingCart = this.getCart();
-    const cartItem = shoppingCart.find(e => e.product.id === item.id);
+    const cartItem = shoppingCart.find(e => e.product.id === item.product.id);
     if (!cartItem) {
       console.log("Objeto no encontrado.");
       return;
