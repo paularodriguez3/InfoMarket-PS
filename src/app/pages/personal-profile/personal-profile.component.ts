d8120ebae28283ab2598cb4d767594f7f01b9d39
kevinjfa <kevin.fa7@outlook.com>
2025-04-29 15:30:51 +0100

    Personal profile ready on Angular, prepared to use firestore service

--- src/app/pages/personal-profile/personal-profile.component.ts
+++ src/app/pages/personal-profile/personal-profile.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
+
+export interface User {
+  name: string,
+  username: string,
+  surname: string,
+  email: string,
+  tlf: string
+}
 
 @Component({
   selector: 'app-personal-profile',
-  standalone: false,
   templateUrl: './personal-profile.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrl: './personal-profile.component.css'
 })
 export class PersonalProfileComponent {
+  router: Router = inject(Router);
 
+    user: User = {
+      name: "Kevin",
+      username: "kevinjfa",
+      surname: "Falcón",
+      email: "kevinjfa@infomarket.es",
+      tlf: "123456789"
+    };
+
+  uploadChanges() {
+    //TODO: use firestore service to upload changes
+  }
+
+  signOut() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/']);
+  }
 }
