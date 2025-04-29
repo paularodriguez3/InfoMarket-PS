b89fc7e63e5a18c222cebc4db89dfa5d5a41ebc2
kevinjfa <kevin.fa7@outlook.com>
2025-04-29 16:16:06 +0100

    Product details working with generic data, waiting for data to be sent from product list

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Product} from '../../models/product.model';
+import {ShoppingCartService} from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  product: Product = {
+    Nombre : "Lavadora multifunción Teka",
+    Descripcion : "Lavadora multifunción muy resistente",
+    Imagen : "https://th.bing.com/th/id/OIP.gwTG8IIrrG0FeBx3FUJUYAHaKB?rs=1&pid=ImgDetMain",
+    Precio : 2000,
+    Caracteristicas: ["Lavadora multifunción", "Resistente"]
+  };
+  cantidad: number = 1;
+  shoppingCartService: ShoppingCartService = inject(ShoppingCartService);
 
+  decrementQty() {
+    if (this.cantidad > 1) {
+      this.cantidad--;
+    }
+  }
+
+  incrementQty() {
+    this.cantidad++;
+  }
 }
