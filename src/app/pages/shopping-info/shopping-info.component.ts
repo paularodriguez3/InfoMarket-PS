52f9475916e66bb612525c74c87526f0b995d301
Jesus Toro <jesus.tdd03@gmail.com>
2025-04-29 16:48:29 +0100

    Started implementing ts for shopping info component.

--- src/app/pages/shopping-info/shopping-info.component.ts
+++ src/app/pages/shopping-info/shopping-info.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ShoppingCartService} from '../../services/shopping-cart.service';
+import {ShoppingCartItem} from '../../models/shopping-cart-item.model';
 
 @Component({
   selector: 'app-shopping-info',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-info.component.css'
 })
 export class ShoppingInfoComponent {
+  shoppingCart: ShoppingCartItem[] = [];
 
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    this.shoppingCart = this.shoppingCartService.getCart();
+  }
+
+  calculateTotalPrice(): number {
+    let total:number = 0;
+    for (let item of this.shoppingCart) {
+      total += item.product.Precio * item.quantity;
+    }
+    return total;
+  }
 }
