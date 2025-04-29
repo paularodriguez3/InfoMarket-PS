dfd3ed843f54ff674c43c4c6999aec512831be56
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-04-29 15:27:02 +0100

    Enable responsive menu toggle for desktop, tablet and mobile

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  isDesktopMenuVisible = false;
+  isMobileMenuVisible = false;
 
+  toggleDesktopMenu(): void {
+    if (window.innerWidth >= 769) {
+      this.isDesktopMenuVisible = !this.isDesktopMenuVisible;
+    }
+  }
+
+  toggleMobileMenu(): void {
+    if (window.innerWidth < 769) {
+      this.isMobileMenuVisible = !this.isMobileMenuVisible;
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (window.innerWidth >= 769) {
+      this.isMobileMenuVisible = false;
+    } else {
+      this.isDesktopMenuVisible = false;
+    }
+  }
 }
