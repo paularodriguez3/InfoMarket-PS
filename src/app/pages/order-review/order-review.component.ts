1c962793d0621078b36c2216dcecd2e282dfe907
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2025-05-16 12:22:38 +0100

    Style fixes

--- src/app/pages/order-review/order-review.component.ts
+++ src/app/pages/order-review/order-review.component.ts
@@ -1,7 +1,6 @@
 // src/app/pages/order-review/order-review.component.ts
 
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import emailjs from 'emailjs-com';
 
 import { ShoppingInfoComponent } from '../../components/shopping-info/shopping-info.component';
 import { OrderReviewTemplateComponent } from '../../components/order-review-template/order-review-template.component';
